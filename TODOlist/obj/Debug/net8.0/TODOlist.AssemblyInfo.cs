97ccf21e35173aef920acfe11d1f32f671e45e84
LadaKashubo <lkashubo@yandex.ru>
2025-11-11 12:53:51 +0300

    master

--- TODOlist/obj/Debug/net8.0/TODOlist.AssemblyInfo.cs
+++ TODOlist/obj/Debug/net8.0/TODOlist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TODOlist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37502426a302ecd4b10b510634ba7eb6e970f1f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+019741a031a219ea4b7e4a64ec14d2a287c8f450")]
 [assembly: System.Reflection.AssemblyProductAttribute("TODOlist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TODOlist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
